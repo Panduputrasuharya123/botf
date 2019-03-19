a24843c6703b30053b9d5c2f6bac5f0a0459362d
Panduputrasuharya123 <48697506+Panduputrasuharya123@users.noreply.github.com>
2019-03-19 11:11:52 +0700

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,75 @@
+import discord
+from discord.ext import commands
+import asyncio
+import time
+
+bot=commands.Bot(command_prefix='D')
+dabs='440281410197258280'
+start=time.time()
+
+@bot.event
+async def on_ready():
+    print('Logged in as:')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('--------------------')
+    await bot.change_presence(game=discord.Game(name='Create By BociL',type=0))
+    
+@bot.command(pass_context=True)
+async def ping():
+    await bot.say(':ping_pong:')
+    await bot.say('You pinged me haha')
+    
+@bot.command(pass_context=True)
+async def mute(ctx,target:discord.Member):
+    if ctx.message.author.id==(dabs):
+      role=discord.utils.get(ctx.message.server.roles,name='nadeko-mute')
+
+      await bot.add_roles(target,role)
+    else:
+        await bot.say('No permission!')
+
+@bot.command(pass_context=True)
+async def warn(ctx,target:discord.Member):
+    if ctx.message.author.id==(dabs):
+      await bot.send_message(target,'Warning!!')
+    else:
+        await bot.say('No permission!')
+        
+@bot.command(pass_context=True)
+async def kick(ctx,target:discord.Member):
+    if ctx.message.author.id==(dabs):
+      await bot.kick(target)
+    else:
+        await bot.say('No permission!')
+
+@bot.command(pass_context=True)
+async def ban(ctx,target:discord.Member):
+    if ctx.message.author.id==(dabs):
+      await bot.ban(target)
+    else:
+        await bot.say('No permission!')
+    
+@bot.command(pass_context=True)
+async def uptime(ctx):
+    now=time.time()
+    sec=int(now-start)
+    mins=int(sec//60)
+    await bot.say(f'Uptime is {sec} seconds!')
+    
+@bot.command(pass_context=True)
+async def clear(ctx,num:int):
+    if ctx.message.author.id==(dabs):
+        await bot.purge_from(ctx.message.channel,limit=num)
+        await bot.say(f'Cleared {num} messages.')
+    else:
+          await bot.say('No permission!')
+          
+@bot.command(pass_context=True)
+async def presence(ctx,text:str,type:int):
+    if ctx.message.author.id==(dabs):
+        await bot.change_presence(game=discord.Game(name=text,type=type))
+    else:
+          await bot.say('No permission!')
+
+bot.run('NTU3MTc4MTYyMzI1NDg3NjM3.D3H57A.BXmNIrqYjRKc77DW4Jc4LpS-JAY')
